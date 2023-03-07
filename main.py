d529a4a03fee88e7c6fc9ae4019c3322ccacd194
Jason Davis <Jasonodavis323@gmail.com>
2023-03-06 19:04:14 -0700

    first try oh yeah

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-
+import requests 
 import os
-print(os.getcwd())
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+endpoint_url = 'https://newsapi.org/v2/top-headlines'
+
+params = {'country':"us"}
+
+response = requests.get(endpoint_url, params=params, headers={"Authorization": api_key})
+
+
+
+if response.status_code == 200:
+    articles = response.json()['articles']
+    headlines = [article['title'] for article in articles]
+    for headline in headlines:
+        print(headline)
+else:
+    print('ERROR getting headlines.. (line 18)')
